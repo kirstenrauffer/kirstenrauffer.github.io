c017bb593a96ebcbcc196a1f967a938201979312
Kirsten <kirstenrauffer@gmail.com>
2020-09-13 16:21:35 -0700

    soooo smooooth

--- surprise.js
+++ surprise.js
@@ -1,18 +1,13 @@
 const WINTER = [
   {
-    src: 'surprises/breakfast.png',
-    alt: `A hearty breakfast`,
-    width: '50',
+    src: 'surprises/cookie.png',
+    alt: `Grandma's cookies`,
+    width: '38',
   },
   {
     src: 'surprises/wine.png',
     alt: `An antique bottle of wine`,
-    width: '58',
-  },
-  {
-    src: 'surprises/cookie.png',
-    alt: `Grandma's cookies`,
-    width: '38',
+    width: '65',
   },
   {
     src: 'surprises/holly.png',
@@ -20,19 +15,14 @@ const WINTER = [
     width: '42',
   },
   {
-    src: 'surprises/pancakes.png',
-    alt: `Yummy pancakes with maple syrup drizzled on top`,
-    width: '50',
+    src: 'surprises/chocolate_cake.png',
+    alt: `A rich chocolate cake`,
+    width: '55',
   },
   {
-    src: 'surprises/snow_yam.png',
-    alt: `A frozen snow yam`,
+    src: 'surprises/coffee.png',
+    alt: `A warm cup of coffee`,
     width: '38',
-  },
-  {
-    src: 'surprises/pumpkin_pie.png',
-    alt: `A delightful pumpkin pie`,
-    width: '50',
   },
   {
     src: 'surprises/rhubarb_pie.png',
@@ -42,6 +32,11 @@ const WINTER = [
   {
     src: 'surprises/soup.png',
     alt: `A hot bowl of clam chowder`,
+    width: '50',
+  },
+  {
+    src: 'surprises/snow.png',
+    alt: `A clump of snow`,
     width: '50',
   }
 ];
@@ -78,14 +73,14 @@ const AUTUMN = [
     width: '50',
   },
   {
-    src: 'surprises/rhubarb_pie.png',
-    alt: `A tart rhubarb pie`,
-    width: '50',
+    src: 'surprises/pumpkin.png',
+    alt: `A plump pumpkin`,
+    width: '60',
   },
   {
     src: 'surprises/soup.png',
-    alt: `A hot bowl of clam chowder`,
-    width: '50',
+    alt: `A fragrant mushroom soup`,
+    width: '45',
   }
 ];
 
@@ -186,16 +181,17 @@ getRandomIntInclusive = (min, max) => {
 }
 
 getSurprise = () => {
+  // return WINTER[getRandomInt(WINTER.length)];
   const today = new Date();
   const month = today.getMonth();
   if (month < 2 || month > 11) {
-    return WINTER[getRandomInt(WINTER.length - 1)];
+    return WINTER[getRandomInt(WINTER.length)];
   } else if (month < 6) {
-    return SPRING[getRandomInt(SPRING.length - 1)];
+    return SPRING[getRandomInt(SPRING.length)];
   } else if (month < 8) {
-    return SUMMER[getRandomInt(SUMMER.length - 1)];
+    return SUMMER[getRandomInt(SUMMER.length)];
   } else {
-    return AUTUMN[getRandomInt(AUTUMN.length - 1)];
+    return AUTUMN[getRandomInt(AUTUMN.length)];
   }
 }
 
@@ -208,6 +204,19 @@ onUnleashSurprise = () => {
   img.classList += 'index__surprise';
   Draggable.create(img, {});
   document.getElementById('interactive-container').appendChild(img);
+
+  animateSurprise(img);
+
+  document.getElementById('aria-live').innerHTML = `${surprise.alt} just appeared!`;
+}
+
+animateSurprise = (img) => {
+  const path1 = document.getElementById('path1-svg');
+  const path2 = document.getElementById('path2-svg');
+  const offset = 90;
+
+  path1.style.left = (offset + getRandomIntInclusive(-40, 40)) + 'px';
+  path2.style.left = (offset + getRandomIntInclusive(-40, 40)) + 'px';
 
   const end = getRandomInt(13)/100;
   const path = getRandomInt(10) > 5 ? '#path1' : '#path2';
@@ -224,6 +233,4 @@ onUnleashSurprise = () => {
       align: path,
     }
   });
-
-  document.getElementById('aria-live').innerHTML = `${surprise.alt} just appeared!`;
 }
