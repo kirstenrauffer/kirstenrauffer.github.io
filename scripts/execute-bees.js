02a81dcf74663937f90dfd4c216251006e61a2de
Kirsten Rauffer <kirstenrauffer@hotmail.com>
2021-11-20 20:34:56 -0600

    yay more

--- scripts/execute-bees.js
+++ scripts/execute-bees.js
@@ -6,8 +6,10 @@ const _executeBees = () => {
   workerBee.onmessage = () => {
     _createBees();
     _createBee('bee1');
+    _createBee('bee2');
     document.addEventListener('mousemove', (event) => {
       _animateBee(document.getElementById('bee1'), event.clientX + 30, event.clientY);
+      _animateBee(document.getElementById('bee2'), event.clientX -30, event.clientY + 30);
     });
   }
 }
@@ -20,7 +22,7 @@ const _createBee = (id) => {
   beeEl.classList = ['main__bee']
   beeEl.setAttribute('aria-hidden', true);
 
-  if (_getRandomInt(window.innerWidth) < window.innerWidth / 3) {
+  if (_getRandomInt(window.innerWidth) < window.innerWidth / 2) {
     beeEl.style.zIndex = 100;
   } else {
     beeEl.style.zIndex = -1;
@@ -39,7 +41,7 @@ const _createBee = (id) => {
 }
 
 const _createBees = () => {
-  for(let i = 0; i < 10; i++) {
+  for(let i = 0; i < 8; i++) {
     const beeEl = _createBee();
     setInterval(_animateBee, _getRandomInt(4500, 5500), beeEl);
   }
@@ -57,9 +59,14 @@ const _animateBee = (beeEl, left = 0, top = 0) =>  {
   beeEl.style.left = `${newLeft}px`;
   beeEl.style.top = `${newTop}px`;
 
+  if (beeEl.id) {
+    debugger;
+  }
+
   if (currentLeft && newLeft > currentLeft) {
     beeEl.style.transform = 'scaleX(-1)';
   } else {
+    debugger;
     beeEl.style.transform = 'scaleX(1)';
   }
 }
